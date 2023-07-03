6d67ad183d44a9f8b73a6dc7f293230904f8b7d6
ashokreddyinfy <1133981.oht33182u6.infy@gmail.com>
2023-07-03 16:11:12 +0530

    initial commit

---
+++ src/main/java/com/infosys/gcp/GoogleTransformingVehicleBackendJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.infosys.gcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleTransformingVehicleBackendJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleTransformingVehicleBackendJavaSpringbootApplication.class, args);
+	}
+
+}
